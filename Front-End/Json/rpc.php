9462b859149619ad478746d8c69826c59f084aa7
hiumi250 <mateusroots2009@hotmail.com>
2018-09-30 16:28:52 -0300

    [ADD] Added full front-end (JSON Messages) archives and path.

---
+++ Front-End/Json/rpc.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class RabbitRpcClient
+{
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection(
+            'localhost',
+            5672,
+            'guest',
+            'guest'
+        );
+        $this->channel = $this->connection->channel();
+        list($this->callback_queue, ,) = $this->channel->queue_declare(
+            "",
+            false,
+            false,
+            true,
+            false
+        );
+        $this->channel->basic_consume(
+            $this->callback_queue,
+            '',
+            false,
+            false,
+            false,
+            false,
+            array(
+                $this,
+                'onResponse'
+            )
+        );
+    }
+
+    public function onResponse($rep)
+    {
+        if ($rep->get('correlation_id') == $this->corr_id) {
+            $this->response = $rep->body;
+        }
+    }
+
+    public function call($n)
+    {
+        $this->response = null;
+        $this->corr_id = uniqid();
+
+        $msg = new AMQPMessage(
+            (string) $n,
+            array(
+                'correlation_id' => $this->corr_id,
+                'reply_to' => $this->callback_queue
+            )
+        );
+        $this->channel->basic_publish($msg, '', 'rpc_queue');
+        while (!$this->response) {
+            $this->channel->wait();
+        }
+        return ($this->response);
+    }
+}
+
+	$jsonRPC = new \stdClass;
+	$jsonRPC->nome = $_POST['nome'];
+	$jsonRPC->telefone = $_POST['telefone'];	
+	$jsonRPC->cpf = $_POST['cpf'];
+	$jsonRPC->email = $_POST['email'];
+	
+	$RPCMessage = json_encode($jsonRPC);
+	
+	$Rabbit_rpc = new RabbitRpcClient();
+	$response = $Rabbit_rpc->call($RPCMessage);
+	echo "<script>
+	alert('$response');
+	window.location.href='http://localhost/RPCJson';
+	</script>";
+?>
